c3ea06a5c211d450b3a46e245b904e077d977fad
yumin <developerjyumin.gmail.com>
2024-03-14 23:12:21 +0900

    start setting

---
+++ src/main/java/com/borisoongeo/AircraftSimuWeb/AircraftSimuWebApplication.java
@@ -0,0 +1,13 @@
+package com.borisoongeo.AircraftSimuWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftSimuWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftSimuWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borisoongeo/AircraftSimuWeb/AircraftSimuWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borisoongeo.AircraftSimuWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftSimuWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
